3f2bd9356de3f016d26e8231aca3384b5909ef3f
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2026-08-16 19:26:04 +0700

    feat(rpi5): initial device tree and partition scaffolding for aosp 15

---
+++ AndroidProducts.mk
@@ -0,0 +1,6 @@
+PRODUCTS_MAKEFILES := \
+	$(LOCAL_DIR)/rpi5.mk
+
+COMMON_LUNCH_CHOICES := \
+	rpi5-userdebug \
+	rpi5-eng
---
+++ rpi5.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+PRODUCT_NAME := rpi5
+PRODUCT_DEVICE := rpi5
+PRODUCT_BRAND := RaspberryPi
+PRODUCT_MANUFACTURER := RaspberryPi
+PRODUCT_MODEL := AOSP 15 on Raspberry Pi 5
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=180 \
+    ro.control_privapp_permissions=enforce
+
+PRODUCT_COPY_FILES += \
+    device/rpi/rpi5/fstab.rpi5:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.rpi5
