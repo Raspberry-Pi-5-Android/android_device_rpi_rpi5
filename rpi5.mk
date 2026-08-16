6ca03d4b797007f45213d4d7c499bed8559bbad6
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2026-08-16 20:49:53 +0700

    feat(graphics): configure mesa3d, drm hwcomposer, and minigbm hal for rpi5

--- rpi5.mk
+++ rpi5.mk
@@ -9,9 +9,20 @@ PRODUCT_MODEL := AOSP 15 on Raspberry Pi 5
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=180 \
+    ro.surface_flinger.has_wide_color_display=false \
+    ro.surface_flinger.has_HDR_display=false \
+    ro.surface_flinger.use_color_management=false \
     ro.control_privapp_permissions=enforce
 
 PRODUCT_COPY_FILES += \
     device/rpi/rpi5/fstab.rpi5:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.rpi5
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator-service.default \
+    android.hardware.graphics.composer-service.default \
+    gralloc.minigbm \
+    hwcomposer.drm \
+    libGLES_mesa \
+    vulkan.broadcom
 
 $(call inherit-product-if-exists, vendor/rpi/rpi5/rpi5-vendor.mk)
